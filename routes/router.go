d3f8796f16bf6b8ceff791d631e45489023c1a19
JrHero14 <overload.emperor14@gmail.com>
2022-03-07 13:47:19 +0800

    Fix Deployment 3

--- routes/router.go
+++ routes/router.go
@@ -4,11 +4,10 @@ import (
 	userRoutes "github.com/Jrhero14/FiberRelation/UserApp/routes"
 	noteRoutes "github.com/Jrhero14/FiberRelation/noteApp/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupRoutes(app *fiber.App) {
-	api := app.Group("/api", logger.New())
+	api := app.Group("/api")
 
 	// Setup the Node Routes
 	noteRoutes.SetupNoteRoutes(api)
