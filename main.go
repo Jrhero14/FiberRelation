f0ab4aa1dab16d95db194c3468425564de3f403e
JrHero14 <overload.emperor14@gmail.com>
2022-03-07 14:09:13 +0800

    Fix Deployment 8

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Jrhero14/FiberRelation/database"
 	"github.com/Jrhero14/FiberRelation/routes"
 	"github.com/gofiber/fiber/v2"
+	"os"
 )
 
 func main() {
@@ -17,5 +18,12 @@ func main() {
 	routes.SetupRoutes(app)
 
 	// Listen on PORT 3000
-	app.Listen(":3000")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	errListen := app.Listen(":" + port)
+	if errListen != nil {
+		os.Exit(1)
+	}
 }
